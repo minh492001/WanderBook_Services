63fe00cc168571c6f3013372568ad807e1608e5f
MinMin <gow.bin.gg@gmail.com>
2024-04-19 22:58:42 +0700

    Create Spring Boot Project

---
+++ src/main/java/com/wander_book/WanderBookServicesApplication.java
@@ -0,0 +1,13 @@
+package com.wander_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderBookServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanderBookServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wander_book/WanderBookServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wander_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderBookServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
