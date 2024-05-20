109f5b6e60abe57ea1211769354fdb23abc52bc8
MinMin <gow.bin.gg@gmail.com>
2024-05-20 23:34:52 +0700

    feat: add feature [Get all rooms]

--- src/main/java/com/wander_book/controller/RoomController.java
+++ src/main/java/com/wander_book/controller/RoomController.java
@@ -1,9 +1,11 @@
 package com.wander_book.controller;
 
+import com.wander_book.exception.PhotoRetrievalException;
 import com.wander_book.model.Room;
 import com.wander_book.response.RoomResponse;
 import com.wander_book.service.IRoomService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +13,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -32,5 +36,35 @@ public class RoomController {
     @GetMapping("/room/types")
     public List<String> getRoomType() {
         return roomService.getAllRoomTypes();
+    }
+
+    @GetMapping("/all-rooms")
+    public ResponseEntity<List<RoomResponse>> getAllRooms() throws SQLException {
+        List<Room> rooms = roomService.getAllRooms();
+        List<RoomResponse> roomResponses = new ArrayList<>();
+        for(Room room : rooms) {
+            byte[] photoBytes = roomService.getRoomPhotoByRoomId(room.getId());
+            if (photoBytes != null && photoBytes.length > 0) {
+                String base64Photo = Base64.encodeBase64String(photoBytes);
+                RoomResponse roomResponse = getRoomResponse(room);
+                roomResponse.setPhoto(base64Photo);
+                roomResponses.add(roomResponse);
+            }
+        }
+        return ResponseEntity.ok(roomResponses);
+    }
+
+    private RoomResponse getRoomResponse(Room room) {
+        byte[] photoBytes = null;
+        Blob photoBlob = room.getPhoto();
+        if (photoBlob != null ) {
+            try {
+                photoBytes = photoBlob.getBytes(1, (int) photoBlob.length());
+            }
+            catch (SQLException e) {
+                throw new PhotoRetrievalException("Error retrieving photo");
+            }
+        }
+        return new RoomResponse(room.getId(), room.getRoomType(), room.getRoomPrice(), room.isBooked(), photoBytes);
     }
 }
--- src/main/java/com/wander_book/response/RoomResponse.java
+++ src/main/java/com/wander_book/response/RoomResponse.java
@@ -2,6 +2,7 @@ package com.wander_book.response;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 
 import java.math.BigDecimal;
 
@@ -18,5 +19,13 @@ public class RoomResponse {
         this.id = id;
         this.roomType = roomType;
         this.roomPrice = roomPrice;
+    }
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice, boolean isBooked, byte[] photoBBytes) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photoBBytes != null ? Base64.encodeBase64String(photoBBytes) : null;
     }
 }
