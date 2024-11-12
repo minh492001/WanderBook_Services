fb8a395018dac50ca30e6227b9fd4aa5c6d81275
sonDew <v.son8697@gmail.com>
2024-11-12 21:33:36 +0700

    adding new

---
+++ src/main/java/com/wander_book/model/booking/BookingStatus.java
@@ -0,0 +1,8 @@
+package com.wander_book.model.booking;
+
+public enum BookingStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELED,
+    COMPLETED
+}
---
+++ src/main/java/com/wander_book/request/SimpleBookingRequest.java
@@ -0,0 +1,23 @@
+package com.wander_book.request;
+
+import com.wander_book.model.booking.BookingStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SimpleBookingRequest {
+    private Long userId;
+    private Long roomId;
+    private Long checkInTimestamp;
+    private Long checkOutTimestamp;
+    private int adultsCount;
+    private int childrenCount;
+    private int totalGuests;
+    private String notes;
+    private BookingStatus status;
+}
