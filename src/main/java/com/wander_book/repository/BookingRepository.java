93abe8d12ae1e241240684c480e2d5745d142ef6
sonDew <v.son8697@gmail.com>
2024-11-12 21:30:54 +0700

    adding new

--- src/main/java/com/wander_book/repository/BookingRepository.java
+++ src/main/java/com/wander_book/repository/BookingRepository.java
@@ -1,12 +1,45 @@
-//package com.wander_book.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public interface BookingRepository extends JpaRepository<BookedRoom, Long> {
-//    List<BookedRoom> findByGuestEmail(String email);
-//
-//    Optional<BookedRoom> findByBookingConfirmationCode(String confirmationCode);
-//}
+package com.wander_book.repository;
+
+import com.wander_book.model.Branch;
+import com.wander_book.model.booking.Booking;
+import com.wander_book.model.booking.BookingStatus;
+import com.wander_book.model.room.Room;
+import com.wander_book.model.user.User;
+import com.wander_book.repository.comon.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookingRepository extends BaseRepository<Booking> {
+
+    // Find booking by confirmation Code
+    Optional<Booking> findByConfirmationCode(String confirmationCode);
+
+    // Find all bookings by user email
+    List<Booking> findByUser_Email(String email);
+
+    // Find all bookings by user
+    List<Booking> findByUser(User user);
+
+    // Find bookings by status
+    List<Booking> findByStatus(BookingStatus status);
+
+    // Find bookings by room
+    List<Booking> findByRoom(Room room);
+
+    // Find Booking has room related with branch
+    List<Booking> findByRoom_Branch(Branch branch);
+
+    // Find active bookings for a specific room during a time period
+    @Query("SELECT b FROM Booking b WHERE b.room = :room AND b.status = 'CONFIRMED' " +
+            "AND b.checkInTimestamp < :end AND b.checkOutTimestamp > :start")
+    List<Booking> findActiveBookingsForRoomDuringPeriod(Room room, Long start, Long end);
+
+    // Find all confirmed bookings for a specific user
+    @Query("SELECT b FROM Booking b WHERE b.user = :user AND b.status = 'CONFIRMED'")
+    List<Booking> findConfirmedBookingsByUser(User user);
+
+    // Count bookings by room and status, useful for checking availability
+    long countByRoomAndStatus(Room room, BookingStatus status);
+}
--- src/main/java/com/wander_book/service/impl/BookingService.java
+++ src/main/java/com/wander_book/service/impl/BookingService.java
@@ -1,79 +1,220 @@
-//package com.wander_book.service.impl;
-//
-//import com.wander_book.exception.InvalidBookingRequestException;
-//import com.wander_book.exception.ResourceNotFoundException;
-//import com.wander_book.model.room.Room;
-//import com.wander_book.repository.BookingRepository;
-//import com.wander_book.service.IBookingService;
-//import com.wander_book.service.IRoomService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class BookingService implements IBookingService {
-//    private final BookingRepository bookingRepository;
-//    private final IRoomService roomService;
-//
-//    @Override
-//    public List<BookedRoom> getAllBookings() {
-//        return bookingRepository.findAll();
-//    }
-//
-//    @Override
-//    public String saveBooking(Long roomId, BookedRoom bookingRequest) {
-//        if (bookingRequest.getCheckOutDate().isBefore(bookingRequest.getCheckInDate())){
-//            throw new InvalidBookingRequestException("Check-in date must come before check-out date");
-//        }
-//        Room room = roomService.getRoomById(roomId).get();
-//        List<BookedRoom> existingBookings = room.getBookings();
-//        boolean roomIsAvailable = roomIsAvailable(bookingRequest, existingBookings);
-//        if (roomIsAvailable) {
-//            room.addBooking(bookingRequest);
-//            bookingRepository.save(bookingRequest);
-//        }
-//        else {
-//            throw new InvalidBookingRequestException("Sorry. This room is not available for the selected dates");
-//        }
-//        return bookingRequest.getBookingConfirmationCode();
-//    }
-//
-//    @Override
-//    public List<BookedRoom> getBookingsByUserEmail(String email) {
-//        return bookingRepository.findByGuestEmail(email);
-//    }
-//
-//    @Override
-//    public void deleteBooking(Long bookingId) {
-//        bookingRepository.deleteById(bookingId);
-//    }
-//
-//    @Override
-//    public BookedRoom findByBookingConfirmationCode(String confirmationCode) {
-//        return bookingRepository.findByBookingConfirmationCode(confirmationCode).orElseThrow(() -> new ResourceNotFoundException("Can not find booking with code : " + confirmationCode));
-//    }
-//
-//    private boolean roomIsAvailable(BookedRoom bookingRequest, List<BookedRoom> existingBookings) {
-//        return existingBookings.stream()
-//                .noneMatch(existingBooking ->
-//                        bookingRequest.getCheckInDate().equals(existingBooking.getCheckInDate())
-//                                || bookingRequest.getCheckOutDate().isBefore(existingBooking.getCheckOutDate())
-//                                || (bookingRequest.getCheckInDate().isAfter(existingBooking.getCheckInDate())
-//                                && bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckOutDate()))
-//                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate())
-//
-//                                && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckOutDate()))
-//                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate())
-//
-//                                && bookingRequest.getCheckOutDate().isAfter(existingBooking.getCheckOutDate()))
-//
-//                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate())
-//                                && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckInDate()))
-//
-//                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate())
-//                                && bookingRequest.getCheckOutDate().equals(bookingRequest.getCheckInDate()))
-//                );
-//    }
-//}
+package com.wander_book.service.impl;
+
+import com.wander_book.model.Branch;
+import com.wander_book.model.booking.Booking;
+import com.wander_book.model.booking.BookingStatus;
+import com.wander_book.model.room.Room;
+import com.wander_book.model.user.User;
+import com.wander_book.repository.BookingRepository;
+import com.wander_book.request.SimpleBookingRequest;
+import com.wander_book.service.Common.BaseServiceImpl;
+import com.wander_book.service.IBookingService;
+import com.wander_book.service.IBranchService;
+import com.wander_book.service.IRoomService;
+import com.wander_book.service.IUserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.wander_book.service.Common.UpdateUtil.*;
+
+@Service
+public class BookingService extends BaseServiceImpl<Booking> implements IBookingService {
+
+    private final BookingRepository bookingRepository;
+    private final IBranchService branchService;
+    private final IRoomService roomService;
+    private final IUserService userService;
+
+
+    public BookingService(BookingRepository bookingRepository, BranchService branchService, RoomService roomService, IUserService userService) {
+        this.roomService = roomService;
+        this.userService = userService;
+        this.repository = bookingRepository;
+        this.bookingRepository = bookingRepository;
+        this.branchService = branchService;
+    }
+
+    @Override
+    public List<Booking> findByUser(User user) {
+        return bookingRepository.findByUser(user);
+    }
+
+    @Override
+    public List<Booking> findByBranch(Long id) {
+        Optional<Branch> branchOptional = branchService.findById(id);
+
+        if (branchOptional.isPresent()) {
+            Branch branch = branchOptional.get();
+            return bookingRepository.findByRoom_Branch(branch);
+        }
+
+        // Return an empty list if branch is not found, or you may throw an exception if preferred
+        return List.of();
+    }
+
+    @Override
+    public List<Booking> findByUserEmail(String email) {
+        return bookingRepository.findByUser_Email(email);
+    }
+
+    @Override
+    public Booking findByConfirmationCode(String confirmationCode) {
+        return bookingRepository.findByConfirmationCode(confirmationCode).orElse(null);
+    }
+
+    @Override
+    public List<Booking> findBookingsByStatus(BookingStatus status) {
+        return bookingRepository.findByStatus(status);
+    }
+
+    @Override
+    public List<Booking> findBookingsByRoom(Room room) {
+        return bookingRepository.findByRoom(room);
+    }
+
+    @Override
+    public List<Booking> findActiveBookingsForRoomDuringPeriod(Room room, Long start, Long end) {
+        if (end - start < 86400000L) {
+            throw new IllegalArgumentException("Start date must be at least 1 day before end date.");
+        }
+
+        return bookingRepository.findActiveBookingsForRoomDuringPeriod(room, start, end);
+    }
+
+    @Override
+    public long countBookingsByRoomAndStatus(Room room, BookingStatus status) {
+        return bookingRepository.countByRoomAndStatus(room, status);
+    }
+
+    @Override
+    public Booking createBooking(SimpleBookingRequest bookingRequest) {
+        if (bookingRequest.getCheckOutTimestamp() - bookingRequest.getCheckInTimestamp() < 86400000L) {
+            throw new IllegalArgumentException("Check-in date must be at least 1 day before check-out date.");
+        }
+
+        Room room = roomService.findById(bookingRequest.getRoomId())
+                .orElseThrow(() -> new EntityNotFoundException("Room not found with id: " + bookingRequest.getRoomId()));
+
+        if ((bookingRequest.getAdultsCount()+bookingRequest.getChildrenCount()) > room.getMaxOccupancy()) {
+            throw new IllegalArgumentException("Total guests cannot exceed room's max occupancy of " + room.getMaxOccupancy());
+        }
+
+        User user = userService.findById(bookingRequest.getUserId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + bookingRequest.getUserId()));
+
+        Booking booking = Booking.builder()
+                .user(user)
+                .room(room)
+                .checkInTimestamp(bookingRequest.getCheckInTimestamp())
+                .checkOutTimestamp(bookingRequest.getCheckOutTimestamp())
+                .adultsCount(bookingRequest.getAdultsCount())
+                .childrenCount(bookingRequest.getChildrenCount())
+                .totalGuests(bookingRequest.getAdultsCount() + bookingRequest.getChildrenCount())
+                .notes(bookingRequest.getNotes())
+                .status(bookingRequest.getStatus() != null ? bookingRequest.getStatus() : BookingStatus.PENDING) // Set default status to PENDING if null
+                .build();
+
+        booking.initializeBooking();
+        return bookingRepository.save(booking);
+    }
+
+    @Override
+    public void confirmBooking(Long bookingId) {
+        Optional<Booking> bookingOptional = bookingRepository.findById(bookingId);
+        if (bookingOptional.isPresent()) {
+            Booking booking = bookingOptional.get();
+            if (!(booking.getStatus() == BookingStatus.PENDING)) {
+                throw new IllegalStateException("Booking is not in the state to be confirmed");
+            }
+            booking.setStatus(BookingStatus.CONFIRMED);
+            booking.getRoom().bookRoom();
+            bookingRepository.save(booking);
+        }
+        throw new EntityNotFoundException("Booking not found with id: " + bookingId);
+    }
+
+    @Override
+    public void cancelBooking(Long bookingId) {
+        Optional<Booking> bookingOptional = bookingRepository.findById(bookingId);
+        if (bookingOptional.isPresent()) {
+            Booking booking = bookingOptional.get();
+            if (booking.getStatus() == BookingStatus.PENDING || booking.getStatus() == BookingStatus.CONFIRMED) {
+                booking.setStatus(BookingStatus.CANCELED);
+                booking.getRoom().cancelBooking();
+                bookingRepository.save(booking);
+                return;
+            }
+            throw new IllegalStateException("Booking is already canceled or completed");
+        }
+        throw new EntityNotFoundException("Booking not found with id: " + bookingId);
+    }
+
+    @Override
+    public Booking updateBooking(Long id, SimpleBookingRequest updateBooking) {
+
+        if (updateBooking.getCheckOutTimestamp() - updateBooking.getCheckInTimestamp() < 86400000L) {
+            throw new IllegalArgumentException("Check-in date must be at least 1 day before check-out date.");
+        }
+
+        Room room = roomService.findById(updateBooking.getRoomId())
+                .orElseThrow(() -> new EntityNotFoundException("Room not found with id: " + updateBooking.getRoomId()));
+
+        if (updateBooking.getTotalGuests() > room.getMaxOccupancy()) {
+            throw new IllegalArgumentException("Total guests cannot exceed room's max occupancy of " + room.getMaxOccupancy());
+        }
+
+        User user = userService.findById(updateBooking.getUserId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + updateBooking.getUserId()));
+
+        Optional<Booking> bookingOptional = bookingRepository.findById(id);
+
+        if (bookingOptional.isPresent()) {
+            Booking booking = bookingOptional.get();
+
+            booking.setRoom(room);
+            booking.setUser(user);
+
+            // Update booking details based on editBookingRequest properties
+            updateIfNotNull(updateBooking.getCheckInTimestamp(), booking::setCheckInTimestamp);
+            updateIfNotNull(updateBooking.getCheckOutTimestamp(), booking::setCheckOutTimestamp);
+            updateIfNotNull(updateBooking.getNotes(), booking::setNotes);
+            updateIfNotNull(updateBooking.getStatus(), booking::setStatus);
+
+            updateIfPositive(updateBooking.getAdultsCount(), booking::setAdultsCount);
+            updateIfNonNegative(updateBooking.getChildrenCount(), booking::setChildrenCount);
+            updateIfPositive(updateBooking.getTotalGuests(), booking::setTotalGuests);
+
+            return bookingRepository.save(booking);
+        }
+        throw new EntityNotFoundException("Booking not found with id: " + id);
+    }
+
+    @Override
+    public Booking extendBooking(Long id, Long newCheckOutTimestamp) {
+        Optional<Booking> bookingOptional = bookingRepository.findById(id);
+        if (bookingOptional.isPresent()) {
+            Booking booking = bookingOptional.get();
+            if (booking.getStatus() == BookingStatus.COMPLETED || booking.getStatus() == BookingStatus.CANCELED) {
+                booking.setCheckOutTimestamp(newCheckOutTimestamp);
+                bookingRepository.save(booking);
+            }
+            throw new IllegalStateException("Booking cannot be extended");
+        }
+        throw new EntityNotFoundException("Booking not found with id: " + id);
+    }
+
+    @Override
+    public void softDeleteById(Long id) {
+        Optional<Booking> bookingOptional = bookingRepository.findById(id);
+        if (bookingOptional.isPresent()) {
+            Booking booking = bookingOptional.get();
+            booking.onDelete();
+            bookingRepository.save(booking);
+        }
+        throw new EntityNotFoundException("Booking not found with id: " + id);
+    }
+
+}
