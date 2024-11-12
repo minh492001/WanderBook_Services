dee125e8f225776409ee1ef423e6ae68ec1325f1
sonDew <v.son8697@gmail.com>
2024-11-12 21:36:30 +0700

    adding new

--- src/main/java/com/wander_book/service/IBookingService.java
+++ src/main/java/com/wander_book/service/IBookingService.java
@@ -1,15 +1,44 @@
-//package com.wander_book.service;
-//
-//import java.util.List;
-//
-//public interface IBookingService {
-//    List<BookedRoom> getAllBookings();
-//
-//    String saveBooking(Long roomId, BookedRoom bookingRequest);
-//
-//    List<BookedRoom> getBookingsByUserEmail(String email);
-//
-//    void deleteBooking(Long bookingId);
-//
-//    BookedRoom findByBookingConfirmationCode(String confirmationCode);
-//}
+package com.wander_book.service;
+
+import com.wander_book.model.booking.Booking;
+import com.wander_book.model.booking.BookingStatus;
+import com.wander_book.model.room.Room;
+import com.wander_book.model.user.User;
+import com.wander_book.request.SimpleBookingRequest;
+import com.wander_book.service.Common.IBaseService;
+
+import java.util.List;
+
+public interface IBookingService extends IBaseService<Booking> {
+
+    List<Booking> findByUser(User user);
+
+    List<Booking> findByBranch(Long id);
+
+    List<Booking> findByUserEmail(String email);
+
+    Booking findByConfirmationCode(String confirmationCode);
+
+    List<Booking> findBookingsByStatus(BookingStatus status);
+
+    List<Booking> findBookingsByRoom(Room room);
+
+    // Find active bookings for a specific room during a time period
+    List<Booking> findActiveBookingsForRoomDuringPeriod(Room room, Long start, Long end);
+
+    // Count bookings by room and status
+    long countBookingsByRoomAndStatus(Room room, BookingStatus status);
+
+    // Create a new booking
+    Booking createBooking(SimpleBookingRequest bookingRequest);
+
+    void confirmBooking(Long bookingId);
+
+    void  cancelBooking(Long bookingId);
+
+    Booking updateBooking(Long id, SimpleBookingRequest updateBooking);
+
+    Booking extendBooking(Long bookingId, Long newCheckOutTimestamp);
+
+    void softDeleteById(Long id);
+}
